22d1e75209fefb07640deb71df87fc979b093def
Dmitrij Chekulaiev <studentpoltntu@gmail.com>
2016-04-12 16:58:28 +0300

    add changes of javaScript for height pages

--- kappe/js/main.js
+++ kappe/js/main.js
@@ -1,12 +1,44 @@
 $(document).ready(function(){
 	function ecvalheight(e){
-		if ($('header').height() < $('content').height()){
-			var height = $('content').height();
-
+		$('header').css('height', 'auto');
+		if ($('header').height() < $('.wrapper').height()){
+			var height = $('.wrapper').height();
 			$('header').css('height',height+'px');
-		
 		}
+
 	}
+
 	$(window).resize(ecvalheight);
 	ecvalheight();
 });
+
+$(document).ready(function(){
+	function ecvalheight(e){
+
+		if ($('iframe').height() < $('.wrapper').height()){
+			var height = $('.wrapper').height();
+
+			$('iframe').css('height',height+'px');
+		}
+
+	}
+
+	$(window).resize(ecvalheight);
+	ecvalheight();
+});
+
+$(document).ready(function() {
+    $(".wrapper").fadeIn(500);
+	$("a.transition").click(function(event){
+		event.preventDefault();
+		linkLocation = this.href;
+		$(".wrapper").fadeOut(300, redirectPage);
+	});
+
+	function redirectPage() {
+		window.location = linkLocation;
+	}
+});
+
+
+
